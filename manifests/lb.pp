61db10d73d9b6897d08ab3851dece48418ab88bf
Hugo van Duijn <hugo.vanduijn@naturalis.nl>
2016-06-28 13:07:30 +0000

    added SSL variables

--- manifests/lb.pp
+++ manifests/lb.pp
@@ -5,18 +5,25 @@
 #
 #
 class linnaeusng::lb (
+  $private,
+  $cert,
+  $cacert,
+  $private_keyname = '/etc/ssl/private/STAR_linnaeus_naturalis_nl_key',
+  $cert_keyname    = '/etc/ssl/certs/STAR_linnaeus_naturalis_nl_pem',
+  $cacert_keyname  = '/etc/ssl/certs/STAR_linnaeus_naturalis_nl_cacert.pem',
+
   $vhost       = { 'default_server'            => { 'proxy' => 'http://default_server',
                                                     'server_name' => ['_'],
                                                     'ssl'         => true,
-                                                    'ssl_cert'    => '/var/lib/puppet/ssl/certs/linnaeus-lb-001.naturalis.nl.pem',
-                                                    'ssl_key'     => '/var/lib/puppet/ssl/private_keys/linnaeus-lb-001.naturalis.nl.pem',
+                                                    'ssl_cert'    => '/etc/ssl/certs/STAR_linnaeus_naturalis_nl_pem',
+                                                    'ssl_key'     => '/etc/ssl/private/STAR_linnaeus_naturalis_nl_key',
                                                     'location_cfg_append' => { 'rewrite' => '^ http://linnaeus.naturalis.nl permanent'},
                                                    },
                    'orchidsnguinea_vhost'      => { 'proxy' => 'http://orchidsnguinea',
                                                     'server_name' => ['orchidsnguinea.linnaeus.naturalis.nl','orchids.naturalis.nl'],
                                                     'ssl'         => true,
-                                                    'ssl_cert'    => '/var/lib/puppet/ssl/certs/linnaeus-lb-001.naturalis.nl.pem',
-                                                    'ssl_key'     => '/var/lib/puppet/ssl/private_keys/linnaeus-lb-001.naturalis.nl.pem',
+                                                    'ssl_cert'    => '/etc/ssl/certs/STAR_linnaeus_naturalis_nl_pem',
+                                                    'ssl_key'     => '/etc/ssl/private/STAR_linnaeus_naturalis_nl_key',
                                                    }
                  },
   $location    = { 'loc_orchidsnguinea_ssl'    => { 'location' => '/linnaeus_ng/admin/',
@@ -30,7 +37,30 @@ class linnaeusng::lb (
 
 ){
 
-  class { 'nginx': }
+# configure SSL 
+
+  file { $private_keyname :
+    ensure  => present,
+    content => $private,
+    mode    => '0600',
+  }
+
+  file { $cert_keyname :
+    ensure  => present,
+    content => $cert,
+    mode    => '0600',
+  }
+
+  file { $cacert_keyname :
+    ensure  => present,
+    content => $cacert,
+    mode    => '0600',
+  }
+
+
+  class { 'nginx':
+    require => [File[$private_keyname],File[$cert_keyname],File[$cacert_keyname]],
+  }
 
 #  create_resources
   create_resources(nginx::resource::vhost,$vhost,{})
