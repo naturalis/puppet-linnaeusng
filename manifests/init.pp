b5d6c8da58c6308b255d972bfeb0e4ec534052fa
Hugo van Duijn <hugo.vanduijn@naturalis.nl>
2018-04-12 14:09:09 +0000

    added password to config and restricted permissions

--- manifests/init.pp
+++ manifests/init.pp
@@ -33,24 +33,28 @@ class role_linnaeusng (
 
   file { '/data/linnaeus/initdb/1_init_db.sql':
     ensure   => file,
+    mode     => '0600',
     content  => template('role_linnaeusng/1_init_db.erb'),
     require  => File['/data/linnaeus/initdb'],
   }
 
   file { '/data/linnaeus/initdb/2_empty_database.sql':
     ensure   => file,
+    mode     => '0600',
     source   => 'puppet:///modules/role_linnaeusng/2_empty_database.sql',
     require  => File['/data/linnaeus/initdb'],
   }
 
   file { '/data/linnaeus/mysqlconf/my-linnaeus.cnf':
     ensure   => file,
-    source   => 'puppet:///modules/role_linnaeusng/my-linnaeus.cnf',
+    mode     => '0600',
+    content  => template('role_linnaeusng/my-linnaeus.cnf.erb'),
     require  => File['/data/linnaeus/mysqlconf'],
   }
 
   file { "${role_linnaeusng::repo_dir}/.env":
     ensure   => file,
+    mode     => '0600',
     content  => template('role_linnaeusng/env.erb'),
     require  => Vcsrepo[$role_linnaeusng::repo_dir],
     notify   => Exec['Restart containers on change'],
